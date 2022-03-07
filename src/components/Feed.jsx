aca5b8dfa959e7c73eb99a96274985f12b95f118
ubong <ubongnt@gmail.com>
2022-03-07 08:39:02 +0100

    Integrating Firebase

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,19 +5,54 @@ import {
   Image,
   Subscriptions,
 } from "@material-ui/icons";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import InputOption from "./InputOption";
+import Post from "./Post";
+import { db } from "../firebase";
+import firebase from "firebase/compat/app";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        );
+      });
+  }, []);
+  const sendPost = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      name: "Ubong Ntekim",
+      description: "This is a test",
+      message: input,
+      photoUrl: "",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput("");
+  };
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
         <div className="feed__input">
           <Create />
           <form>
-            <input type="text" />
-            <button type="submit">Send</button>
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              type="text"
+            />
+            <button onClick={sendPost} type="submit">
+              Send
+            </button>
           </form>
         </div>
         <div className="feed__inputOptions">
@@ -31,6 +66,15 @@ const Feed = () => {
           />
         </div>
       </div>
+      {posts.map(({ id, data }) => (
+        <Post
+          key={id}
+          name={data.name}
+          description={data.description}
+          message={data.message}
+          photoUrl={data.photoUrl}
+        />
+      ))}
     </div>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,8 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyBUe2z-HA_SoQdWHdEkezU9CJaQWuV5Ztg",
   authDomain: "social-app-e247e.firebaseapp.com",
@@ -6,3 +11,11 @@ const firebaseConfig = {
   messagingSenderId: "657369666901",
   appId: "1:657369666901:web:0eca43e24e542436cf0bdc",
 };
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const auth = firebase.auth()
+const db = firebaseApp.firestore()
+
+
+export { auth, db}
